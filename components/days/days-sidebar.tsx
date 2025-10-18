b38db8015d6925f8a08bde48fa090d4fd947d042
Kent0710 <https://github.com/Eduard-K-A/temp-project-launchpad>
2025-10-18 09:45:59 +0800

    perf(days-sidebar): memoitized the dates for pathname tracking and active styling

--- components/days/days-sidebar.tsx
+++ components/days/days-sidebar.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import { Description, SubTitle } from "../texts";
-import {BentoContainer} from "../bento-container";
+import { BentoContainer } from "../bento-container";
 import DaysSidebarItem from "./days-sidebar-item";
 import { Calendar } from "lucide-react";
 import { useDates } from "@/context/dates-context";
 import { ScrollArea } from "../ui/scroll-area";
 import { DateType } from "@/lib/types";
+import CustomDateRangerPicker from "./custom-date-range-picker";
+import { usePathname } from "next/navigation";
 
 interface DaysSidebarProps {
     className?: string;
@@ -24,6 +26,17 @@ const DaysSidebar: React.FC<DaysSidebarProps> = ({
         setDates(formattedDates);
     }, [formattedDates, setDates]);
 
+    const pathname = usePathname();
+
+    // Memoitized the dates list to prevent unnecessary re-renders
+    // Also check if the pathname equals the date and add an active class
+    const datesMemo = useMemo(() => {
+        return dates.map((date) => ({
+            ...date,
+            isActive: pathname === `/day/${date.text}`,
+        }));
+    }, [dates, pathname]);
+
     return (
         <BentoContainer
             className={`flex flex-col ${className} 
@@ -32,10 +45,13 @@ const DaysSidebar: React.FC<DaysSidebarProps> = ({
             `}
         >
             <header className="border-b pb-2 mb-2">
-                <div className="flex items-center gap-2">
-                    <Calendar size={20} />
-                    <SubTitle> Choose Day </SubTitle>
-                </div>
+                <section className="flex items-center justify-between">
+                    <div className="flex items-center gap-2">
+                        <Calendar size={20} />
+                        <SubTitle> Choose Day </SubTitle>
+                    </div>
+                    <CustomDateRangerPicker />
+                </section>
                 <Description className="text-background">
                     Select a day to view records.
                 </Description>
@@ -43,17 +59,24 @@ const DaysSidebar: React.FC<DaysSidebarProps> = ({
 
             <main>
                 <nav>
-                    <ScrollArea className="h-[calc(100vh-16rem)] transition pr-2" type="always">
+                    <ScrollArea
+                        className="h-[calc(100vh-16rem)] transition pr-2"
+                        type="always"
+                    >
                         <ul className="text-sm">
-                            {dates.map((date) => (
+                            {datesMemo.map((date) => (
                                 <DaysSidebarItem
                                     key={date.id}
                                     date={date}
-                                    className="
+                                    className={`
                                         text-white font-medium p-2 rounded-md mr-2 my-1
                                         hover:bg-accent hover:text-muted-foreground hover:cursor-pointer hover:pl-4
                                         transition-all
-                                    "
+                                        ${
+                                            date.isActive
+                                                ? "bg-accent text-primary hover:text-primary pl-4"
+                                                : ""
+                                        }`}
                                 />
                             ))}
                         </ul>
