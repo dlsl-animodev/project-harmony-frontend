2553d4fe635d9f0c658b90e5f99e35947cc2d4a0
Kent0710 <https://github.com/Eduard-K-A/temp-project-launchpad>
2025-10-24 17:22:42 +0800

    style(frontend): improve student page loader component

--- components/student/student-on-date.tsx
+++ components/student/student-on-date.tsx
@@ -21,8 +21,8 @@ import {
 } from "@/components/reusables/bento-container";
 import { Description, SubTitle, Title } from "@/components/reusables/texts";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import Loader from "../reusables/loader";
 import { useEffect, useState } from "react";
+import { Skeleton } from "../ui/skeleton";
 
 interface StudentOnDateProps {
     studentId: string;
@@ -53,13 +53,29 @@ const StudentOnDate: React.FC<StudentOnDateProps> = ({ studentId, date }) => {
 
     if (loading) {
         return (
-            <div className="w-full h-full flex items-center justify-center bg-background">
-                <Loader
-                    className="w-full bg-background h-full"
-                    mainText="Loading record data..."
-                    subText="Please wait while we fetch the records"
-                />
-            </div>
+            <BentoContainer className="flex flex-col h-full w-full space-y-8 bg-transparent border-none">
+                <BentoContainerHeader>
+                    <Title>
+                        Student Record for {studentId} on{" "}
+                        {formatDateForRender(date)}
+                    </Title>
+                    <Description>
+                        If data is missing, it means the student did not check
+                        in or out on that date. Refresh the page if you believe
+                        this is an error.
+                    </Description>
+                </BentoContainerHeader>
+                <section className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                    {[1, 2, 3].map((item) => (
+                        <Skeleton
+                            key={item}
+                            className="h-[3rem] sm:h-[10rem] flex gap-10 sm:gap-0 sm:flex-col "
+                        />
+                      
+                    ))}
+                </section>
+                <Skeleton className="h-1/2 w-full" />
+            </BentoContainer>
         );
     }
 
