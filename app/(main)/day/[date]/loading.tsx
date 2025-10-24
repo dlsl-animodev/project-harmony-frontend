8a2512e2f70a283a3b278e1d3ec2b815f7afa9e2
Kent0710 <https://github.com/Eduard-K-A/temp-project-launchpad>
2025-10-24 17:25:27 +0800

    style(frontend): improve single date page loader component

--- app/(main)/day/[date]/loading.tsx
+++ app/(main)/day/[date]/loading.tsx
@@ -1,13 +1,12 @@
-import Loader from "@/components/reusables/loader";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Loading = () => {
     return (
-        <div className="flex h-full w-full">
-            <Loader
-                className="w-full bg-background"
-                mainText="Loading record data..."
-                subText="Please wait while we fetch the records"
-            />
+        <div className="flex flex-col h-full w-full p-4">
+            <Skeleton className="h-10 w-1/4 mb-4" />
+            <Skeleton className="h-6 w-1/4 mb-4" />
+            <Skeleton className="flex-1 w-full" />
+            
         </div>
     );
 };
