1d333f5692c31230da568a766b174efa0c220459
Kent0710 <https://github.com/Eduard-K-A/temp-project-launchpad>
2025-10-24 14:09:06 +0800

    style(frontend): styling for loading.tsx in pages

--- app/(main)/day/range/loading.tsx
+++ app/(main)/day/range/loading.tsx
@@ -1,9 +1,15 @@
+import Loader from "@/components/reusables/loader";
+
 const Loading = () => {
     return (
-        <div>
-            Loading...
+        <div className="w-full h-full flex items-center justify-center bg-background">
+            <Loader
+                className="w-full bg-background h-full"
+                mainText="Loading record data..."
+                subText="Please wait while we fetch the records"
+            />
         </div>
-    )
+    );
 };
 
 export default Loading;
