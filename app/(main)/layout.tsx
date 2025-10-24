66e19af2e8eccaef4e7615684db0970e8fd5187c
Kent0710 <https://github.com/Eduard-K-A/temp-project-launchpad>
2025-10-24 15:08:09 +0800

    feat(frontend): add suspense loading fallback in main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,7 +5,8 @@ import Header from "@/components/header";
 import DaysSidebarServer from "@/components/days/days-sidebar-server";
 import { Toaster } from "sonner";
 import NextTopLoader from "nextjs-toploader";
-import React from "react";
+import React, { Suspense } from "react";
+import Loader from "@/components/reusables/loader";
 
 export const dynamic = "force-dynamic";
 
@@ -20,14 +21,24 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
                 <div className="flex flex-col h-[100dvh] overflow-hidden">
                     <NextTopLoader />
                     <Header />
-                    <main className="flex-1 overflow-y-auto flex flex-col lg:flex-row gap-4 lg:p-8">
-                        <DatesProvider>
-                            <DaysSidebarServer className="lg:basis-[20%] shrink-0" />
-                            <section className="flex-1 flex flex-col">
-                                {children}
-                            </section>
-                        </DatesProvider>
-                    </main>
+                    <Suspense
+                        fallback={
+                            <Loader
+                                className="h-full bg-transparent"
+                                mainText="Welcome to Project Harmony Dashboard!"
+                                subText="Please wait while we set up things for you"
+                            />
+                        }
+                    >
+                        <main className="flex-1 overflow-y-auto flex flex-col lg:flex-row gap-4 lg:p-8">
+                            <DatesProvider>
+                                <DaysSidebarServer className="lg:basis-[20%] shrink-0" />
+                                <section className="flex-1 flex flex-col">
+                                    {children}
+                                </section>
+                            </DatesProvider>
+                        </main>
+                    </Suspense>
                     <Toaster />
                 </div>
             </SidebarOpenProvider>
