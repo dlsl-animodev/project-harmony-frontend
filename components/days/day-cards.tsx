f63b123841a3f3b25d20b9ba978bfb842f901161
Kent0710 <https://github.com/Eduard-K-A/temp-project-launchpad>
2025-10-25 03:11:50 +0800

    fix(frontend): fix day-card layout

--- components/days/day-cards.tsx
+++ components/days/day-cards.tsx
@@ -31,7 +31,7 @@ interface DayCardsProps {
 
 const DayCards: React.FC<DayCardsProps> = ({ children }) => {
     return (
-        <ul className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
+        <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-4">
             {children}
         </ul>
     );
