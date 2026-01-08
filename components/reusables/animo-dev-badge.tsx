da364106a934e1441973628d4f556b5cf53ff6ae
Kent0710 <christian_kent_bayani@dlsl.edu.ph>
2026-01-08 14:33:14 +0800

    feat: new centered landing page on desktop view

--- components/reusables/animo-dev-badge.tsx
+++ components/reusables/animo-dev-badge.tsx
@@ -1,9 +1,17 @@
+import { twMerge } from "tailwind-merge";
+
 import { Disc3 } from "lucide-react";
 import Link from "next/link";
 
-const AnimoDevBadge = () => {
+interface AnimoDevBadgeProps {
+    className?: string;
+}
+
+const AnimoDevBadge: React.FC<AnimoDevBadgeProps> = ({
+    className,
+}) => {
     return (
-        <div className="text-white flex items-center rounded-lg text-sm text-center gap-4 px-4 py-2 shadow-md bg-gradient-to-r from-yellow-500 via-purple-500 to-pink-500 font-bold border">
+        <div className={twMerge(`text-white flex items-center rounded-lg text-sm text-center gap-4 px-4 py-2 shadow-md bg-gradient-to-r from-yellow-500 via-purple-500 to-pink-500 font-bold border`, className)}>
             <Disc3 className=" animate-spin size-8 md:size-5" />{" "}
             <span>
                 Developed and maintained by
