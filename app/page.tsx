4f6ba4476ca01af99084db56fb8ba35788c098f7
Kent0710 <christian_kent_bayani@dlsl.edu.ph>
2026-01-08 14:33:14 +0800

    feat: new centered landing page on desktop view

--- app/page.tsx
+++ app/page.tsx
@@ -1,93 +1,74 @@
-import { Description, Title } from "@/components/reusables/texts";
+import { Title, Description } from "@/components/reusables/texts";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Book } from "lucide-react";
-import AudioWave from "../components/reusables/audio-wave";
-import ANIMODEVLOGO from "@/public/animo-dev-logo.jpg";
 import Image from "next/image";
+import ANIMODEVLOGO from "@/public/animo-dev-logo.jpg";
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import Link from "next/link";
+import { Book } from "lucide-react";
 import AnimoDevBadge from "@/components/reusables/animo-dev-badge";
 
-const LandingPage = () => {
+const TestLandingPage = () => {
     return (
-        <div className="h-full">
-            <main className="h-[100dvh] py-5 px-10 md:px-14 relative">
-                {/* HERO SECTION  */}
-                <section className="flex flex-col justify-between h-full ">
-                    <div className="flex justify-between">
-                        <div className="flex gap-4 flex-col lg:flex-row">
-                            <div className="flex space-x-2">
-                                <Image
-                                    src={ANIMODEVLOGO}
-                                    alt="animo-dev-logo"
-                                    width={1024}
-                                    height={768}
-                                    className="w-[3rem] h-[3rem] md:w-[4rem] md:h-[4rem] lg:w-[5rem]  lg:h-[5rem] rounded-lg border shadow-md"
-                                />
-                                <div className="w-[3rem] h-[3rem] md:w-[4rem] md:h-[4rem] lg:w-[5rem]  lg:h-[5rem] rounded-lg border shadow-md flex items-center text-sm text-center font-semibold">
-                                    {" "}
-                                    musikalista logo{" "}
-                                </div>
-                            </div>
-                        </div>
-                        <span className="flex gap-2 mt-2 lg:mt-0">
-                            <Link href={"/signIn"}>
-                                <Button variant={"outline"}>Sign In</Button>
-                            </Link>
-                            <AboutAndLegals />
-                        </span>
-                    </div>
-
-                    <div className="mt-20 md:mt-0 space-y-10">
-                        <div className="w-full flex items-center justify-center md:justify-start lg:justify-start mb-4">
-                            <AnimoDevBadge />
-                        </div>
-                        <Title className="text-5xl sm:text-6xl lg:text-7xl mb-4 font-bold break-words text-center md:text-left">
-                            Never
-                            <span className="text-primary">
-                                {" "}
-                                Lose Track{" "}
-                            </span>{" "}
-                            of a Beat, <br className="hidden lg:block" /> or
-                            <span className="text-primary"> Who is </span>
-                            in the
-                            <span className="text-primary"> Room.</span>{" "}
-                        </Title>
-                        <Description className="text-sm  md:text-xl lg:text-2xl font-medium text-center md:text-left">
-                            The official website for the Iot-powered Musikalista
-                            Room Tracker <br className="hidden md:block " />{" "}
-                            that monitors check-ins and check-outs.
-                        </Description>
-                        <Description className="text-xs md:text-sm lg:text-base text-center md:text-left">
-                            Musikalista IoT-powered Room Tracker monitors every
-                            entry and exit in the Music Club Room,{" "}
-                            <br className="hidden md:block " /> syncing data to
-                            this website&apos;s dashboard so club heads and
-                            members always know who is practicing,{" "}
-                            <br className="hidden md:block " /> when the
-                            room&apos;s occupied, and who last used it, all
-                            without manual logs.
-                        </Description>
-
-                        <div className="flex items-center justify-between">
-                            <Link href={"/home"}>
-                                <Button> Get started </Button>
-                            </Link>
-                            <AudioWave />
-                        </div>
+        <div className="h-dvh w-dvw">
+            <header className="flex items-center justify-between px-8 h-[8rem]">
+                {/* Left Side  */}
+                <section className="flex gap-4 items-center">
+                    <Image
+                        src={ANIMODEVLOGO}
+                        alt="animo-dev-logo"
+                        width={1024}
+                        height={768}
+                        className="w-[2rem] h-[2rem] md:w-[3rem] md:h-[3rem] lg:w-[4rem] lg:h-[4rem] rounded-lg border shadow-md"
+                    />
+                    <div className="w-[2rem] h-[2rem] md:w-[3rem] md:h-[3rem] lg:w-[4rem] lg:h-[4rem] rounded-lg border shadow-md flex items-center text-sm text-center font-semibold">
+                        {" "}
+                        musikalista logo{" "}
                     </div>
                 </section>
+                {/* Right Side */}
+                <section className="flex gap-4 items-center">
+                    <Link href={"/signIn"}>
+                        <Button size={"md"} variant={"outline"}>
+                            Sign in
+                        </Button>
+                    </Link>
+                    <AboutAndLegals />
+                </section>
+            </header>
+            <main className="flex flex-col gap-6 items-center h-[calc(100dvh-16rem)] justify-center">
+                <div className="flex flex-col gap-4 items-center ">
+                    <AnimoDevBadge className="w-fit" />
+                    <Title className="text-5xl sm:text-6xl lg:text-7xl mb-4 font-bold break-words text-center">
+                        Never Lose Track of a Beat, <br /> or Who is in the
+                        Room.
+                    </Title>
+                    <Description className="text-sm  md:text-xl lg:text-2xl font-medium text-center">
+                        The official website for the Iot-powered Musikalista
+                        Room Tracker that monitors check-ins and check-outs.
+                    </Description>
+                </div>
+                <Description className="text-xs md:text-sm lg:text-base text-center">
+                    Musikalista IoT-powered Room Tracker monitors every entry
+                    and exit in the Music Club Room, syncing data to this
+                    website&apos;s dashboard <br /> so club heads and members
+                    always know who is practicing, when the room&apos;s
+                    occupied, and who last used it, all without manual logs.
+                </Description>
+
+                <Link href={"/home"}>
+                    <Button size={"lg"}> Get started </Button>
+                </Link>
             </main>
         </div>
     );
 };
 
-export default LandingPage;
+export default TestLandingPage;
 
 const AboutAndLegals = () => {
     const items = [
@@ -99,14 +80,14 @@ const AboutAndLegals = () => {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant={"outline"}>
+                <Button variant={"outline"} size={"md"}>
                     {" "}
                     <Book />{" "}
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
                 {items.map((item) => (
-                    <DropdownMenuItem key={item.href} asChild>
+                    <DropdownMenuItem key={item.label} asChild>
                         <Link href={item.href}>{item.label}</Link>
                     </DropdownMenuItem>
                 ))}
